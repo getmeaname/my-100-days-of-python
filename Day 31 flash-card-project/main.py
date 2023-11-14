00382ab333c892676b94cfe91f4ccb01124ebdb0
Arun <arun.idk@outlook.com>
2023-11-14 20:26:08 +0530

    Completed successfully.

--- Day 31 flash-card-project/main.py
+++ Day 31 flash-card-project/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import pandas
 import random
+
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
 to_learn = {}
@@ -13,24 +14,29 @@ except FileNotFoundError:
 else:
     to_learn = data.to_dict(orient="records")
 
+
 def next_card():
-    global current_card,flip_timer
+    global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_side)
     flip_timer = window.after(3000, func=flip_card)
+
+
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_side)
 
+
 def is_known():
     to_learn.remove(current_card)
-    data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv", index=False)
+    new_data = pandas.DataFrame(to_learn)
+    new_data.to_csv("data/words_to_learn.csv", index=False)
     next_card()
+
 
 window = Tk()
 window.title("Flashy")
